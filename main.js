0e08144118452a09578a83ce6b8b15642445bbee
dcorso21 <david.l.corson@gmail.com>
2020-09-24 17:27:05 -0400

    populating grid sucessfully

--- main.js
+++ main.js
@@ -1,6 +1,29 @@
 var sudoku1 = document.querySelector(".sudoku1");
 var sudoku2 = document.querySelector(".sudoku2");
 var updated = true;
+var solvedGrid = [];
+var entryGrid = [];
+
+
+
+//  Found https://www.javascripttutorial.net/dom/manipulating/remove-all-child-nodes/
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+function oddSquare(x,y) {
+    let topBottom = [0,1,2,6,7,8]
+    let middle = [3,4,5]
+    if (topBottom.includes(x) && topBottom.includes(y)){
+        return true
+    }
+    if (middle.includes(x) && middle.includes(y)){
+        return true
+    }
+    return false
+}
 
 class sudoku {
     constructor() {
@@ -12,7 +35,9 @@ class sudoku {
         let sq, p, f, i;
         sq = document.createElement("div");
         sq.setAttribute("id", `(${x}, ${y})`);
-        sq.setAttribute("class", "square");
+        let cl = "square";
+        cl = oddSquare(x, y) ? cl + " emphasis": cl;
+        sq.setAttribute("class", cl);
         p = document.createElement("p");
         p.textContent = " ";
         sq.appendChild(p);
@@ -28,27 +53,42 @@ class sudoku {
             }
             updated = false;
             let value;
-            console.log(p);
             value = p.textcontent == undefined ? " " : p.textContent;
             sq.removeChild(p);
             i.setAttribute("value", value);
             sq.appendChild(f);
             i.focus();
         });
+
         f.addEventListener("submit", (e) => {
-            e.preventDefault();
-            p.textContent = `${e.target[0].value}`;
-            sq.appendChild(p);
-            sq.removeChild(f);
-            updated = true;
-        });
-        f.addEventListener("focusout", () => {
             if (sq.contains(p)) {
                 return;
+            }
+            e.preventDefault();
+            if (console.log(s.possible(Number(sq.id[1]), Number(sq.id[4]), Number(e.target[0].value)))){
+                p.textContent = `${e.target[0].value}`;
+            } else {
+                alert("Entry is not valid")
             }
             sq.appendChild(p);
             sq.removeChild(f);
             updated = true;
+            solveCurrent()
+        });
+        
+        f.addEventListener("focusout", (e) => {
+            if (sq.contains(p)){return;}
+            let val = e.target.value;
+            // if (s.possible(sq.id[1], sq.id[4], e.target[0].value)){
+            //     p.textContent = `${e.target[0].value}`;
+            // } else {
+            //     alert("Entry is not valid")
+            // }
+            p.textContent = val;
+            sq.appendChild(p);
+            sq.removeChild(f);
+            updated = true;
+            solveCurrent()
         });
         return sq;
     }
@@ -61,26 +101,49 @@ class sudoku {
         }
         return sudoku;
     }
+
+    pullEntryGrid(sudoku) {
+        entryGrid = [];
+        let row = [];
+        let squares = sudoku.getElementsByClassName("square");
+        for (let i = 0; i < squares.length; i++) {
+            let n = squares[i].getElementsByTagName("p")[0].textContent;
+            n = Number(n);
+            if (![1, 2, 3, 4, 5, 6, 7, 8, 9].includes(n)) {
+                n = 0;
+            }
+            n = Number(n);
+            row.push(n);
+            if (row.length === 9) {
+                entryGrid.push(row);
+                row = [];
+            }
+        }
+    }
 }
 
 class solver {
     constructor() {
-        this.grid = [];
+        // this.grid = [];
+        this.solved = [];
     }
 
     square(x, y) {
         let sq, p, f, i;
         sq = document.createElement("div");
         sq.setAttribute("id", `s(${x}, ${y})`);
-        sq.setAttribute("class", "square");
+        let cl = "square";
+        cl = oddSquare(x, y) ? cl + " emphasis": cl;
+        sq.setAttribute("class", cl);
         p = document.createElement("p");
-        p.textContent = this.grid[x][y];
+        p.textContent = solvedGrid[y][x];
         sq.appendChild(p);
         return sq;
     }
 
     makeSolution(sudoku) {
-        for (let row = 0; row < this.grid.length; row++) {
+        removeAllChildNodes(sudoku);
+        for (let row = 0; row < solvedGrid.length; row++) {
             for (let col = 0; col < 9; col++) {
                 sudoku.appendChild(this.square(col, row));
             }
@@ -88,70 +151,55 @@ class solver {
         return sudoku;
     }
 
-    createGrid(sudoku) {
-        this.grid = [];
-        let row = [];
-        let squares = sudoku.getElementsByClassName("square");
-        for (let i = 0; i < squares.length; i++) {
-            let n = squares[i].getElementsByTagName("p")[0].textContent;
-            row.push(n);
-            if (row.length === 9) {
-                this.grid.push(row);
-                row = [];
-            }
-        }
-    }
     possible(x, y, n) {
-        // return true
+        if (solvedGrid[y].includes(n)) {
+            return false;
+        }
         for (let i = 0; i < 9; i++) {
-            // Check for same value in row
-            if (this.grid[y][i] === n) {
-                return false;
-                // Check for same value in Column
-            } else if (this.grid[i][x] === n) {
+            if (solvedGrid[i][x] === n) {
                 return false;
             }
         }
-        // return true
-        let x_sq_origin, y_sq_origin; // This checks for the same value in the square
-        x_sq_origin = Math.floor(x / 3) * 3;
-        y_sq_origin = Math.floor(y / 3) * 3;
-        for (let i = 0; i > 3; i++) {
-            for (let j = 0; i > 3; i++) {
-                if (grid[x_sq_origin + i][y_sq_origin + j] === n) {
-                    return false;
-                }
-            }
+        let x0 = Math.floor(x / 3) * 3,
+            y0 = Math.floor(y / 3) * 3;
+
+        let sq = solvedGrid[y0].slice(x0, x0 + 3);
+        sq.concat(solvedGrid[y0 + 1].slice(x0, x0 + 3));
+        sq.concat(solvedGrid[y0 + 2].slice(x0, x0 + 3));
+        if (sq.includes(n)) {
+            return false;
         }
         return true;
     }
 
-    ssolve() {
-        console.log('here');
-        let AcceptableNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-        for (let x = 0; x < 9; x++) {
-            for (let y = 0; y < 9; y++) {
-                if (!AcceptableNumbers.includes(this.grid[x][y])) {
-                    for (let n = 1; n < 10; n++) {
-                        if (this.possible(x, y, n)) {
-                            this.grid[x][y] = n;
-                            this.ssolve();
-                            this.grid[x][y] = " ";
+    solve() {
+        for (let y = 0; y < 9; y++) {
+            if (solvedGrid[y].includes(0)) {
+                let x = solvedGrid[y].indexOf(0);
+                for (let n = 1; n < 10; n++) {
+                    if (this.possible(x, y, n)) {
+                        solvedGrid[y][x] = n;
+                        if (this.solve() != false) {
+                            return solvedGrid;
                         }
+                        solvedGrid[y][x] = 0;
                     }
-                    return;
                 }
+                return false;
             }
         }
+        return solvedGrid;
     }
 }
 
 const sdku = new sudoku();
+const s = new solver();
 sudoku1 = sdku.makeSquares(sudoku1);
 
-const s = new solver();
-s.createGrid(sudoku1);
-// s.ssolve();
-// console.log(s.grid);
-
-// s.makeSolution(sudoku2);
+function solveCurrent() {
+    solvedGrid = [...entryGrid]
+    sdku.pullEntryGrid(sudoku1)
+    // console.log("unsolved", grid);
+    s.solve();
+    s.makeSolution(sudoku2);
+}
--- test.py
+++ test.py
@@ -7,66 +7,45 @@ grid = [
     [8, 0, 0, 0, 6, 0, 0, 0, 3],
     [4, 0, 0, 8, 0, 3, 0, 0, 1],
     [7, 0, 0, 0, 2, 0, 0, 0, 6],
-    [0, 6, 0, 0, 2, 0, 2, 8, 0],
-    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
     [0, 0, 0, 4, 1, 9, 0, 0, 5],
     [0, 0, 0, 0, 8, 0, 0, 7, 9],
 ]
 
-def findCell(grid, x, y):
-    for yp in range(y,9):
-        if 0 in grid[yp]:
-            return (grid[yp].index(0), yp)
 
-
-
-
-def findNextCellToFill(grid, x, y):
-    for xp in range(x, 9):
-        for yp in range(y, 9):
-            if grid[yp][xp] == 0:
-                return xp, yp
-    for xp in range(0, x):
-        for yp in range(0, y):
-            if grid[yp][xp] == 0:
-                return xp, yp
-    return -1, -1
+# https://stackoverflow.com/questions/1697334/algorithm-for-solving-sudoku
 
 
 def isValid(grid, x, y, n):
     # Check if row doesnt have e as value yet
-    if n in grid[y]: 
+    if n in grid[y]:
         return False
     if n in [grid[i][x] for i in range(9)]:
         return False
 
-    # finding the top left x,y co-ordinates of the section containing the i,j cell
-    firstSquareX = (x//3)*3
-    firstSquareY = (y//3)*3
+    sqX = (x//3)*3
+    sqY = (y//3)*3
 
-    square = grid[firstSquareY][firstSquareX:firstSquareX+3]
-    square.extend(grid[firstSquareY+1][firstSquareX:firstSquareX+3])
-    square.extend(grid[firstSquareY+2][firstSquareX:firstSquareX+3])
+    square = grid[sqY][sqX:sqX+3]
+    square.extend(grid[sqY+1][sqX:sqX+3])
+    square.extend(grid[sqY+2][sqX:sqX+3])
     if n in square:
         return False
-    # print("valid")
     return True
 
 
-def solveSudoku(grid, x=0, y=0):
-    x, y = findNextCellToFill(grid, x, y)
-    # print(x, y)
-    if x == -1:
-        return True
-    for n in range(1, 10):
-        # print('hello')
-        if isValid(grid, x, y, n):
-            grid[y][x] = n
-            if solveSudoku(grid, x, y) != False:
-                return grid
-            # Undo the current cell for backtracking
-            grid[y][x] = 0
-    return False
+def solveSudoku(grid):        
+    for y in range(9):
+        if 0 in grid[y]:
+            x = grid[y].index(0)
+            for n in range(1, 10):
+                if isValid(grid, x, y, n):
+                    grid[y][x] = n
+                    if solveSudoku(grid) != False:
+                        return grid
+                    grid[y][x] = 0
+            return False
+    return grid
 
 
 input = [
@@ -80,4 +59,4 @@ input = [
     [7, 0, 3, 4, 0, 0, 5, 6, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
 ]
-# print(np.matrix(solveSudoku(input)))
+print(np.matrix(solveSudoku(grid)))
